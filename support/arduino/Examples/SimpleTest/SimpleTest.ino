daff754d419cb73de6cee103470d3a5984042e76
Vincenzo Pacella <shaduz@shaduzlabs.com>
2019-03-31 23:15:33 +0200

    update arduino config files

--- support/arduino/Examples/SimpleTest/SimpleTest.ino
+++ support/arduino/Examples/SimpleTest/SimpleTest.ino
@@ -5,8 +5,9 @@
         ##      ##
 ##########      ############################################################# shaduzlabs.com #####*/
 
+#include "default_monospace_7.h"
+
 #include <rastr.h>
-#include <default_monospace_7.h>
 
 //--------------------------------------------------------------------------------------------------
 
@@ -15,7 +16,7 @@ sl::rastr::CanvasMonochrome<128,64> canvas;
 //--------------------------------------------------------------------------------------------------
 
 void setup(){
-  canvas.putText(8, 8, "Hello world", {0xff}, k_font_DefaultMonospace_7);
+  canvas.putText(8, 8, "Hello world!", {0xff}, k_font_DefaultMonospace_7);
 }
 
 void loop() {
---
+++ support/arduino/Examples/SimpleTest/default_monospace_7.h
@@ -0,0 +1,218 @@
+//--------------------------------------------------------------------------------------------------
+// Font: Default Monospace
+// Size: 7
+// Line height: 9
+// Number of chars: 94
+//--------------------------------------------------------------------------------------------------
+
+// clang-format off
+
+// bitmap data
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c32[] = {0x00, 0x00, 0x00, 0x00, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c33[] = {0x42, 0x10, 0x84, 0x01, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c34[] = {0x52, 0x94, 0x00, 0x00, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c35[] = {0x00, 0x15, 0xF5, 0x7D, 0x40};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c36[] = {0x23, 0xA8, 0xE2, 0xB8, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c37[] = {0x45, 0x54, 0x45, 0x54, 0x40};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c38[] = {0x22, 0xA8, 0x8A, 0xC9, 0xA0};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c39[] = {0x42, 0x10, 0x00, 0x00, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c40[] = {0x11, 0x10, 0x84, 0x10, 0x40};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c41[] = {0x41, 0x04, 0x21, 0x11, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c42[] = {0x11, 0xC4, 0x00, 0x00, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c43[] = {0x01, 0x09, 0xF2, 0x10, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c44[] = {0x00, 0x00, 0x00, 0x08, 0x40};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c45[] = {0x00, 0x00, 0x0F, 0x80, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c46[] = {0x00, 0x00, 0x00, 0x00, 0x40};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c47[] = {0x00, 0x44, 0x44, 0x40, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c48[] = {0x74, 0x6B, 0x58, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c49[] = {0x23, 0x08, 0x42, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c50[] = {0x74, 0x44, 0x44, 0x7C, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c51[] = {0xF8, 0x88, 0x28, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c52[] = {0x94, 0xA5, 0xF1, 0x08, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c53[] = {0xFC, 0x3C, 0x18, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c54[] = {0x74, 0x3D, 0x18, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c55[] = {0xF8, 0x44, 0x44, 0x40, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c56[] = {0x74, 0x5D, 0x18, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c57[] = {0x74, 0x62, 0xF0, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c58[] = {0x00, 0x08, 0x00, 0x00, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c59[] = {0x00, 0x08, 0x02, 0x11, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c60[] = {0x00, 0xD9, 0x06, 0x0C, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c61[] = {0x00, 0x1E, 0x07, 0x80, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c62[] = {0x06, 0x0C, 0x13, 0x60, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c63[] = {0x64, 0x88, 0x40, 0x10, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c64[] = {0x74, 0x67, 0x59, 0x45, 0xC0};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c65[] = {0x22, 0xA3, 0xF8, 0xC4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c66[] = {0xF4, 0x7D, 0x18, 0xF8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c67[] = {0x74, 0x61, 0x08, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c68[] = {0xF4, 0x63, 0x18, 0xF8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c69[] = {0xFC, 0x3F, 0x08, 0x7C, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c70[] = {0xFC, 0x3D, 0x08, 0x40, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c71[] = {0x74, 0x61, 0x38, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c72[] = {0x8C, 0x7F, 0x18, 0xC4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c73[] = {0x71, 0x08, 0x42, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c74[] = {0x38, 0x84, 0x29, 0x30, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c75[] = {0x8C, 0xB9, 0x49, 0x44, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c76[] = {0x84, 0x21, 0x08, 0x7C, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c77[] = {0x8E, 0xEB, 0x58, 0xC4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c78[] = {0x8C, 0x73, 0x59, 0xC4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c79[] = {0x74, 0x63, 0x18, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c80[] = {0xF4, 0x7D, 0x08, 0x40, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c81[] = {0x74, 0x63, 0x59, 0x34, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c82[] = {0xF4, 0x7D, 0x49, 0x44, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c83[] = {0x7C, 0x1C, 0x10, 0xF8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c84[] = {0xF9, 0x08, 0x42, 0x10, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c85[] = {0x8C, 0x63, 0x18, 0xB8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c86[] = {0x8C, 0x63, 0x15, 0x10, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c87[] = {0x8C, 0x6B, 0x5A, 0xA8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c88[] = {0x8A, 0x88, 0xA8, 0xC4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c89[] = {0x8C, 0x54, 0x42, 0x10, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c90[] = {0xF8, 0x44, 0x44, 0x7C, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c91[] = {0xE4, 0x21, 0x08, 0x70, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c92[] = {0x04, 0x10, 0x41, 0x04, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c93[] = {0x38, 0x42, 0x10, 0x9C, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c94[] = {0x22, 0x80, 0x00, 0x00, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c95[] = {0x00, 0x00, 0x00, 0x03, 0xE0};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c96[] = {0x82, 0x00, 0x00, 0x00, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c97[] = {0x03, 0x04, 0xE9, 0x30, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c98[] = {0x84, 0x39, 0x29, 0x70, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c99[] = {0x00, 0x1D, 0x08, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c100[] = {0x10, 0x9D, 0x29, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c101[] = {0x03, 0x25, 0xE8, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c102[] = {0x32, 0x38, 0x84, 0x20, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c103[] = {0x03, 0x24, 0xE1, 0x09, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c104[] = {0x84, 0x39, 0x29, 0x48, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c105[] = {0x20, 0x08, 0x42, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c106[] = {0x10, 0x0C, 0x21, 0x09, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c107[] = {0x84, 0xA9, 0x8A, 0x48, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c108[] = {0x61, 0x08, 0x42, 0x38, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c109[] = {0x00, 0x15, 0x5A, 0xD4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c110[] = {0x00, 0x3D, 0x18, 0xC4, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c111[] = {0x00, 0x19, 0x29, 0x30, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c112[] = {0x00, 0x19, 0x2F, 0x42, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c113[] = {0x00, 0x19, 0x2F, 0x08, 0x40};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c114[] = {0x00, 0x2C, 0x94, 0x20, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c115[] = {0x01, 0x90, 0x41, 0x30, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c116[] = {0x42, 0x38, 0x84, 0x20, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c117[] = {0x00, 0x25, 0x29, 0x30, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c118[] = {0x00, 0x23, 0x15, 0x10, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c119[] = {0x00, 0x01, 0x5A, 0xA8, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c120[] = {0x04, 0x54, 0x45, 0x44, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c121[] = {0x00, 0x25, 0x26, 0x22, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c122[] = {0x00, 0x3C, 0x44, 0x78, 0x00};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c123[] = {0x22, 0x11, 0x04, 0x20, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c124[] = {0x21, 0x08, 0x42, 0x10, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c125[] = {0x20, 0x84, 0x11, 0x08, 0x80};
+const PROGMEM uint8_t k_font_DefaultMonospace_7_c126[] = {0x00, 0x11, 0x51, 0x00, 0x00};
+
+
+// character data
+const PROGMEM sl::rastr::Character k_font_DefaultMonospace_7_chars[] = {
+  //                W    H    XOff YOff XAdv Data
+  /* ' ' (0x20) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c32[0]},
+  /* '!' (0x21) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c33[0]},
+  /* '"' (0x22) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c34[0]},
+  /* '#' (0x23) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c35[0]},
+  /* '$' (0x24) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c36[0]},
+  /* '%' (0x25) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c37[0]},
+  /* '&' (0x26) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c38[0]},
+  /* ''' (0x27) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c39[0]},
+  /* '(' (0x28) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c40[0]},
+  /* ')' (0x29) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c41[0]},
+  /* '*' (0x2A) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c42[0]},
+  /* '+' (0x2B) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c43[0]},
+  /* ',' (0x2C) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c44[0]},
+  /* '-' (0x2D) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c45[0]},
+  /* '.' (0x2E) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c46[0]},
+  /* '/' (0x2F) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c47[0]},
+  /* '0' (0x30) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c48[0]},
+  /* '1' (0x31) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c49[0]},
+  /* '2' (0x32) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c50[0]},
+  /* '3' (0x33) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c51[0]},
+  /* '4' (0x34) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c52[0]},
+  /* '5' (0x35) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c53[0]},
+  /* '6' (0x36) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c54[0]},
+  /* '7' (0x37) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c55[0]},
+  /* '8' (0x38) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c56[0]},
+  /* '9' (0x39) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c57[0]},
+  /* ':' (0x3A) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c58[0]},
+  /* ';' (0x3B) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c59[0]},
+  /* '<' (0x3C) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c60[0]},
+  /* '=' (0x3D) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c61[0]},
+  /* '>' (0x3E) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c62[0]},
+  /* '?' (0x3F) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c63[0]},
+  /* '@' (0x40) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c64[0]},
+  /* 'A' (0x41) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c65[0]},
+  /* 'B' (0x42) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c66[0]},
+  /* 'C' (0x43) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c67[0]},
+  /* 'D' (0x44) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c68[0]},
+  /* 'E' (0x45) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c69[0]},
+  /* 'F' (0x46) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c70[0]},
+  /* 'G' (0x47) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c71[0]},
+  /* 'H' (0x48) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c72[0]},
+  /* 'I' (0x49) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c73[0]},
+  /* 'J' (0x4A) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c74[0]},
+  /* 'K' (0x4B) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c75[0]},
+  /* 'L' (0x4C) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c76[0]},
+  /* 'M' (0x4D) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c77[0]},
+  /* 'N' (0x4E) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c78[0]},
+  /* 'O' (0x4F) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c79[0]},
+  /* 'P' (0x50) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c80[0]},
+  /* 'Q' (0x51) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c81[0]},
+  /* 'R' (0x52) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c82[0]},
+  /* 'S' (0x53) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c83[0]},
+  /* 'T' (0x54) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c84[0]},
+  /* 'U' (0x55) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c85[0]},
+  /* 'V' (0x56) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c86[0]},
+  /* 'W' (0x57) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c87[0]},
+  /* 'X' (0x58) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c88[0]},
+  /* 'Y' (0x59) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c89[0]},
+  /* 'Z' (0x5A) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c90[0]},
+  /* '[' (0x5B) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c91[0]},
+  /* '\' (0x5C) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c92[0]},
+  /* ']' (0x5D) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c93[0]},
+  /* '^' (0x5E) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c94[0]},
+  /* '_' (0x5F) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c95[0]},
+  /* '`' (0x60) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c96[0]},
+  /* 'a' (0x61) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c97[0]},
+  /* 'b' (0x62) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c98[0]},
+  /* 'c' (0x63) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c99[0]},
+  /* 'd' (0x64) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c100[0]},
+  /* 'e' (0x65) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c101[0]},
+  /* 'f' (0x66) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c102[0]},
+  /* 'g' (0x67) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c103[0]},
+  /* 'h' (0x68) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c104[0]},
+  /* 'i' (0x69) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c105[0]},
+  /* 'j' (0x6A) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c106[0]},
+  /* 'k' (0x6B) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c107[0]},
+  /* 'l' (0x6C) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c108[0]},
+  /* 'm' (0x6D) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c109[0]},
+  /* 'n' (0x6E) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c110[0]},
+  /* 'o' (0x6F) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c111[0]},
+  /* 'p' (0x70) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c112[0]},
+  /* 'q' (0x71) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c113[0]},
+  /* 'r' (0x72) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c114[0]},
+  /* 's' (0x73) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c115[0]},
+  /* 't' (0x74) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c116[0]},
+  /* 'u' (0x75) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c117[0]},
+  /* 'v' (0x76) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c118[0]},
+  /* 'w' (0x77) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c119[0]},
+  /* 'x' (0x78) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c120[0]},
+  /* 'y' (0x79) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c121[0]},
+  /* 'z' (0x7A) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c122[0]},
+  /* '{' (0x7B) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c123[0]},
+  /* '|' (0x7C) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c124[0]},
+  /* '}' (0x7D) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c125[0]},
+  /* '~' (0x7E) */ {5,   7,   0,   7,   6,   &k_font_DefaultMonospace_7_c126[0]},
+};
+
+// font
+const PROGMEM sl::rastr::Font k_font_DefaultMonospace_7 = {
+    /* name and size:   */ {"Default Monospace", 7},
+    /* bits/pixel:      */ 1,
+    /* line height:     */ 9,
+    /* first character: */ 32,
+    /* last character:  */ 126,
+    &k_font_DefaultMonospace_7_chars[0]
+};
+
+// clang-format on
